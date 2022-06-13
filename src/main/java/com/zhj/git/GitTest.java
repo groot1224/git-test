f297ef98f5710234cb8400b4b5d9cf6796b3e143
zhj <zhj@163.com>
2022-06-13 13:32:08 +0800

    first commit

---
+++ src/main/java/com/zhj/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zhj.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author zhuhejian
+ * @Date 2022/6/13 13:23
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
